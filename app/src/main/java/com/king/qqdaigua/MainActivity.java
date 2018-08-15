6a70dd12a6632fc6e5107bb862a54e1fcc19baaf
qq576777915 <1@h5.gg>
2018-08-15 14:54:03 +0800

    backup

--- app/src/main/java/com/king/qqdaigua/MainActivity.java
+++ app/src/main/java/com/king/qqdaigua/MainActivity.java
@@ -87,55 +87,54 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         getSupportActionBar().setHomeButtonEnabled(true);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close){
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+
             @Override
-            public void onDrawerOpened(View drawerView) {
-                Log.e("123123","打开了");
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0及以上
-                    View decorView = getWindow().getDecorView();
-                    int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-                    decorView.setSystemUiVisibility(option);
-                    //根据上面设置是否对状态栏单独设置颜色
-                    if (useThemestatusBarColor) {
-                        getWindow().setStatusBarColor(getResources().getColor(R.color.zise));
-                    } else {
-                        getWindow().setStatusBarColor(Color.TRANSPARENT);
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+//                Log.e("距离", "" + slideOffset);
+                if (slideOffset > 0) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0及以上
+                        View decorView = getWindow().getDecorView();
+                        int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+                        decorView.setSystemUiVisibility(option);
+                        //根据上面设置是否对状态栏单独设置颜色
+                        if (useThemestatusBarColor) {
+                            getWindow().setStatusBarColor(getResources().getColor(R.color.zise));
+                        } else {
+                            getWindow().setStatusBarColor(Color.TRANSPARENT);
+                        }
+                    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {//4.4到5.0
+                        WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+                        localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
                     }
-                } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {//4.4到5.0
-                    WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
-                    localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !useStatusBarColor) {//android6.0以后可以对状态栏文字颜色和图标进行修改
+                        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                    }
+                    super.onDrawerOpened(drawerView);
+                } else {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0及以上
+                        View decorView = getWindow().getDecorView();
+                        int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+                        decorView.setSystemUiVisibility(option);
+                        //根据上面设置是否对状态栏单独设置颜色
+                        if (useThemestatusBarColor) {
+                            getWindow().setStatusBarColor(getResources().getColor(R.color.blue_title));
+                        } else {
+                            getWindow().setStatusBarColor(Color.TRANSPARENT);
+                        }
+                    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {//4.4到5.0
+                        WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+                        localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+                    }
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !useStatusBarColor) {//android6.0以后可以对状态栏文字颜色和图标进行修改
+                        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                    }
                 }
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !useStatusBarColor) {//android6.0以后可以对状态栏文字颜色和图标进行修改
-                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-                }
-                super.onDrawerOpened(drawerView);
+                super.onDrawerSlide(drawerView, slideOffset);
             }
 
-            // 菜单关闭
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                Log.e("123123","关闭了");
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0及以上
-                    View decorView = getWindow().getDecorView();
-                    int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-                    decorView.setSystemUiVisibility(option);
-                    //根据上面设置是否对状态栏单独设置颜色
-                    if (useThemestatusBarColor) {
-                        getWindow().setStatusBarColor(getResources().getColor(R.color.blue_title));
-                    } else {
-                        getWindow().setStatusBarColor(Color.TRANSPARENT);
-                    }
-                } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {//4.4到5.0
-                    WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
-                    localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
-                }
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !useStatusBarColor) {//android6.0以后可以对状态栏文字颜色和图标进行修改
-                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-                }
-                super.onDrawerClosed(drawerView);
-            }
         };
         drawer.addDrawerListener(toggle);
 //        drawer.setDrawerListener(toggle);
@@ -296,7 +295,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
 
 
         int id = item.getItemId();
